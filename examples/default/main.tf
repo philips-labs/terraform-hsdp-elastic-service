752dd9dbb0bb8a4fd833df14e0bc1091a4be1364
Raj Shivaram <raj.shivaram@hotmail.com>
2022-04-25 20:56:18 +0530

    Updated Main.tf in examples
    Added TFvars example file and made correction to the module name in examples

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,4 +1,4 @@
-module "elasticsearch" {
+module "elastic-service" {
   source       = "philips-labs/elastic-service/hsdp"
   
   // es7-standard-3 is the default plan selected in this module. This field can be overrided with the plan which is suitable as per your requirements
